8cf8892cae19261faf42f55d601604be8a506412
Romy Gaus <romygaus@air-von-romy.localdomain>
2023-07-02 18:34:00 +0200

    reverse scrolling

--- src/App.js
+++ src/App.js
@@ -6,18 +6,45 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      deltaY: 0,
+      maxDelta: 0
     }
   }
 
   componentDidMount () {
-    var bodyDiv = document.getElementsByTagName("body")[0];
-    window.scrollTo(0, bodyDiv.scrollHeight);
     window.history.scrollRestoration = 'manual';
+    window.addEventListener("wheel", this.scrollHandler);
+    window.addEventListener("DOMMouseScroll", this.scrollHandler);
   }
 
+  scrollHandler = event => {
+    this.setState(state => {
+      let newDeltaY = state.deltaY + event.deltaY;
+
+      newDeltaY = Math.max(0, newDeltaY);
+      newDeltaY = Math.min(this.state.maxDelta, newDeltaY);
+
+      return {
+        deltaY: newDeltaY
+      };
+    });
+  };
+
+  updateMaxDelta = () => {
+    var containerDiv = document.getElementsByClassName("container")[0];
+    const maxDelta = containerDiv.scrollHeight - window.innerHeight;
+    this.setState({
+      maxDelta
+    });
+  };
+
   render() {
+    let style_first = {
+      transform: `translateY(-${this.state.maxDelta - this.state.deltaY}px)`
+    };
+
     return (
-      <div className='container'>
+      <div className='container' style={style_first} onLoad={this.updateMaxDelta} >
         <div className='header'>
           <h1>Romy Gaus</h1>
           <NavBar></NavBar>
