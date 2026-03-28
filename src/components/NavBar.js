78baa0ceb578e0aadf2c55caa9594a44d9f6368c
Romy Gaus <romy.p.gaus@gmail.com>
2026-03-28 20:58:16 +0100

    new ref for resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NavBar extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       isActive: false,
-    }
+    };
   }
 
   toggleNavBar = () => {
     this.setState({
-      isActive: !this.state.isActive
-    })
+      isActive: !this.state.isActive,
+    });
   };
 
   render() {
     return (
-      <nav className={this.state.isActive ? 'active' : ''} id="nav">
+      <nav className={this.state.isActive ? "active" : ""} id="nav">
         <ul>
-          <li><a href="https://github.com/RomyGaus" target='_blank' rel='noreferrer'>GitHub</a></li>
-          <li><a href="https://www.linkedin.com/in/romy-gaus-46615b272/" target='_blank' rel='noreferrer'>LinkedIn</a></li>
-          <li><a href="https://www.youtube.com/@RomyGaus" target='_blank' rel='noreferrer'>YouTube</a></li>
-          <li><a href="https://github.com/RomyGaus/personal-website/blob/main/src/resume.pdf" target='_blank' rel='noreferrer'>Resume</a></li>
+          <li>
+            <a
+              href="https://github.com/RomyGaus"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/romy-gaus-46615b272/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@RomyGaus"
+              target="_blank"
+              rel="noreferrer"
+            >
+              YouTube
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/RomyGaus/personal-website/blob/main/src/Romy_Gaus_Scrum_Master_CV.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
+          </li>
         </ul>
         <button className="icon" id="toggle" onClick={this.toggleNavBar}>
           <div className="line line1"></div>
           <div className="line line2"></div>
         </button>
       </nav>
-    )
+    );
   }
 }
 
